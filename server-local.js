4e148e53c1551f6b9f3b3d08f07ccd0015da87ed
Iyawat <iyawat@yahoo.com>
2022-02-08 20:36:03 +0700

    Add thai Lotto API

--- server-local.js
+++ server-local.js
@@ -1,13 +1,13 @@
 "use strict";
 const bodyParser = require("body-parser");
 
-// const app = require('./express/server');
+const app = require("./express/server");
 
-// app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(3000, () => console.log("Local app listening on port 3000!"));
 
-const server = require("./express/server");
-const { PORT = 3000 } = process.env;
+// const server = require("./express/server");
+// const { PORT = 3000 } = process.env;
 
-server
-  .use(bodyParser.json())
-  .listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+// server
+//   .use(bodyParser.json())
+//   .listen(PORT, () => console.log(`App listening on port ${PORT}!`));
